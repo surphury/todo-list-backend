c2083c001541ca129455ba078d57da129fedef1b
José Berríos <surphury@gmail.com>
2022-05-02 16:48:09 -0400

    test: add delete function

--- src/test.rs
+++ src/test.rs
@@ -8,8 +8,7 @@ use mongo::connect;
 use super::*;
 
 #[actix_web::test]
-#[ignore = "requires MongoDB instance running"]
-async fn test() {
+async fn can_add_new_users() {
 	let client = connect().await;
 	// Clear any data currently in the users collection.
 	client
@@ -23,7 +22,8 @@ async fn test() {
 		App::new()
 			.app_data(web::Data::new(client))
 			.service(add_user)
-			.service(get_user),
+			.service(get_user)
+			.service(delete_user),
 	)
 	.await;
 
@@ -34,7 +34,7 @@ async fn test() {
 	};
 
 	let req = TestRequest::post()
-		.uri("/add_user")
+		.uri("/user")
 		.set_form(&user)
 		.to_request();
 
@@ -42,9 +42,60 @@ async fn test() {
 	assert_eq!(response, Bytes::from_static(b"user added"));
 
 	let req = TestRequest::get()
-		.uri(&format!("/get_user/{}", &user.username))
+		.uri(&format!("/user/{}", &user.username))
 		.to_request();
 
 	let response: User = call_and_read_body_json(&app, req).await;
 	assert_eq!(response, user);
+
+	let req = TestRequest::delete()
+		.uri("/user")
+		.set_form(&user)
+		.to_request();
+
+	let response: User = call_and_read_body_json(&app, req).await;
+	assert_eq!(response, user);
+}
+
+#[actix_web::test]
+async fn can_delete_users() {
+	let client = connect().await;
+	// Clear any data currently in the users collection.
+	client
+		.database(DB_NAME)
+		.collection::<User>(COLL_NAME)
+		.drop(None)
+		.await
+		.expect("drop collection should succeed");
+
+	let app = init_service(
+		App::new()
+			.app_data(web::Data::new(client))
+			.service(add_user)
+			.service(get_user)
+			.service(delete_user),
+	)
+	.await;
+
+	let user = User {
+		username: "janedoe".into(),
+		email: "example@example.com".into(),
+		password: "password".into(),
+	};
+
+	let req = TestRequest::post()
+		.uri("/user")
+		.set_form(&user)
+		.to_request();
+
+	let response = call_and_read_body(&app, req).await;
+	assert_eq!(response, Bytes::from_static(b"user added"));
+
+	let req = TestRequest::delete()
+		.uri("/user")
+		.set_form(&user)
+		.to_request();
+
+	let response = call_and_read_body(&app, req).await;
+	assert_eq!(response, Bytes::from_static(b"user deleted"));
 }
