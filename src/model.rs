5e4aa1667d706c0e49b01372fc33b38b1f85e941
José Berríos <surphury@gmail.com>
2022-05-16 11:39:36 -0400

    feat: add `NewUser` struct for new users

--- src/model.rs
+++ src/model.rs
@@ -1,10 +1,17 @@
 use serde::{Deserialize, Serialize};
 
-/* #[derive(Clone, Debug, PartialEq, Deserialize, Serialize)]
-pub struct DisableUser {
+#[derive(Clone, Debug, PartialEq, Deserialize, Serialize)]
+pub struct UnconfirmedUser {
 	pub username: String,
 	pub email: String,
-} */
+	pub key: String,
+}
+
+#[derive(Clone, Debug, PartialEq, Deserialize, Serialize)]
+pub struct NewUser {
+	pub username: String,
+	pub email: String,
+}
 
 #[derive(Clone, Debug, PartialEq, Deserialize, Serialize)]
 pub struct User {
