286802ec29213f0d50bd2b9798e9eec95b3a5b9c
José Berríos <surphury@gmail.com>
2022-05-20 05:05:53 -0400

    feat: migrate from mongodb to mysql

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,39 @@
+mod confirm;
+mod database;
+mod model;
 #[cfg(test)]
 mod test;
 
-mod confirm;
-mod model;
-mod mongo;
+use dotenv::dotenv;
+
+use database::{
+	connect, create_table, get_user_by_username, get_users, insert_new_user, update_password,
+};
 
 use confirm::send_confirmation_email;
 
 use uuid::Uuid;
 
-use std::io::Result;
+use std::io;
 
 use actix_web::{
 	delete, get, post,
 	web::{Data, Json, Path},
 	App, HttpResponse, HttpServer,
 };
-use mongo::connect;
-use mongodb::{bson::doc, Client, Collection};
+
+use sqlx::{mysql::MySqlPool, pool::PoolConnection, MySql};
+
+/* use mongodb::{bson::doc, Client, Collection}; */
+
+/* use std::env; */
+use std::result::Result;
 
 use model::{NewUser, UnconfirmedUser};
 
-const DB_NAME: &str = "market";
-const COLL_NAME: &str = "new_users";
-
 /// Adds a new user to the "users" collection in the database.
 #[post("/user")]
-async fn add_user(client: Data<Client>, form: Json<NewUser>) -> HttpResponse {
+async fn add_user(form: Json<NewUser>, pool: Data<MySqlPool>) -> HttpResponse {
 	let key = Uuid::new_v4();
 	let user = UnconfirmedUser {
 		email: form.email.clone(),
@@ -41,53 +48,49 @@ async fn add_user(client: Data<Client>, form: Json<NewUser>) -> HttpResponse {
 			}
 		}
 	}
-	let collection = client.database(DB_NAME).collection(COLL_NAME);
-	let result = collection.insert_one(user, None).await;
-	match result {
-		Ok(_) => HttpResponse::Ok().body("user added"),
+	let id = insert_new_user(user, pool).await;
+	HttpResponse::Ok().body("user added")
+	/* match result {
+		Ok(_) => ,
 		Err(err) => HttpResponse::InternalServerError().body(err.to_string()),
-	}
+	} */
 }
 
 #[delete("/user")]
-async fn delete_user(client: Data<Client>, form: Json<NewUser>) -> HttpResponse {
-	let collection = client.database(DB_NAME).collection::<NewUser>(COLL_NAME);
+async fn delete_user(form: Json<NewUser>, pool: Data<MySqlPool>) -> HttpResponse {
+	/* let collection = client.database(DB_NAME).collection::<NewUser>(COLL_NAME);
 	let result = collection
 		.find_one_and_delete(doc! { "username" : &form.username }, None)
 		.await;
 	match result {
 		Ok(_) => HttpResponse::Ok().body("user deleted"),
 		Err(err) => HttpResponse::InternalServerError().body(err.to_string()),
-	}
+	} */
+	HttpResponse::Ok().body("user deleted")
 }
 
 /// Gets the user with the supplied username.
 #[get("/user/{username}")]
-async fn get_user(client: Data<Client>, username: Path<String>) -> HttpResponse {
-	let username = username.into_inner();
-	let collection: Collection<NewUser> = client.database(DB_NAME).collection(COLL_NAME);
-	match collection
-		.find_one(doc! { "username": &username }, None)
-		.await
-	{
-		Ok(Some(user)) => HttpResponse::Ok().json(user),
-		Ok(None) => {
-			HttpResponse::NotFound().body(format!("No user found with username {}", username))
-		}
-		Err(err) => HttpResponse::InternalServerError().body(err.to_string()),
-	}
+async fn get_user(username: Path<String>, pool: Data<MySqlPool>) -> HttpResponse {
+	let user = get_user_by_username(username.to_string(), pool).await;
+
+	HttpResponse::Ok().json(user)
 }
 
 #[actix_web::main]
-async fn main() -> Result<()> {
-	let client = connect().await;
+async fn main() -> Result<(), io::Error> {
+	dotenv().ok();
 	let port: u16 = match std::env::var("PORT") {
 		Ok(port) => port.parse::<u16>().unwrap(),
 		Err(_error) => 8080,
 	};
+	let database_url: String = std::env::var("DATABASE_URL").unwrap();
+	let pool: MySqlPool = connect(database_url)
+		.await
+		.expect("Could not connect to database");
 	HttpServer::new(move || {
 		App::new()
-			.app_data(Data::new(client.clone()))
+			.app_data(Data::new(pool.clone()))
 			.service(add_user)
 			.service(get_user)
 			.service(delete_user)
