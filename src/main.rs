ddd430f12f3f964e0b2990e5b94ff99123f00123
José Berríos <surphury@gmail.com>
2022-07-19 15:14:10 -0400

    feat: separate routes

--- src/main.rs
+++ src/main.rs
@@ -1,101 +1,36 @@
-mod confirm;
+#[macro_use]
+extern crate rocket;
+
 mod database;
+mod hashing;
 mod model;
-#[cfg(test)]
-mod test;
+mod routes;
 
-use dotenv::dotenv;
+/* #[cfg(test)]
+mod test; */
 
-use database::{
-	connect, create_table, get_user_by_username, get_users, insert_new_user, update_password,
-};
+use database::connect;
 
-use confirm::send_confirmation_email;
+use routes::{get_tasks, login, post_task, register_user};
 
-use uuid::Uuid;
+use model::Db;
 
-use std::io;
+use sqlx::MySqlPool;
 
-use actix_web::{
-	delete, get, post,
-	web::{Data, Json, Path},
-	App, HttpResponse, HttpServer,
-};
+use std::env::var;
 
-use sqlx::{mysql::MySqlPool, pool::PoolConnection, MySql};
+#[rocket::main]
+async fn main() -> Result<(), rocket::Error> {
+    dotenv::dotenv().ok();
+    let pool: MySqlPool = connect(&var("DATABASE_URL").expect("DATABASE_URL needs to be set"))
+        .await
+        .expect("Could not connect with database");
 
-/* use mongodb::{bson::doc, Client, Collection}; */
+    let _rocket = rocket::build()
+        .manage(Db { pool })
+        .mount("/api", routes![register_user, login, get_tasks, post_task])
+        .launch()
+        .await?;
 
-/* use std::env; */
-use std::result::Result;
-
-use model::{NewUser, UnconfirmedUser};
-
-/// Adds a new user to the "users" collection in the database.
-#[post("/user")]
-async fn add_user(form: Json<NewUser>, pool: Data<MySqlPool>) -> HttpResponse {
-	let key = Uuid::new_v4();
-	let user = UnconfirmedUser {
-		email: form.email.clone(),
-		username: form.username.clone(),
-		key: key.to_string(),
-	};
-	loop {
-		match send_confirmation_email(&user).await {
-			Ok(_) => break,
-			Err(err) => {
-				println!("{:?}", err)
-			}
-		}
-	}
-	let id = insert_new_user(user, pool).await;
-	HttpResponse::Ok().body("user added")
-	/* match result {
-		Ok(_) => ,
-		Err(err) => HttpResponse::InternalServerError().body(err.to_string()),
-	} */
-}
-
-#[delete("/user")]
-async fn delete_user(form: Json<NewUser>, pool: Data<MySqlPool>) -> HttpResponse {
-	/* let collection = client.database(DB_NAME).collection::<NewUser>(COLL_NAME);
-	let result = collection
-		.find_one_and_delete(doc! { "username" : &form.username }, None)
-		.await;
-	match result {
-		Ok(_) => HttpResponse::Ok().body("user deleted"),
-		Err(err) => HttpResponse::InternalServerError().body(err.to_string()),
-	} */
-	HttpResponse::Ok().body("user deleted")
-}
-
-/// Gets the user with the supplied username.
-#[get("/user/{username}")]
-async fn get_user(username: Path<String>, pool: Data<MySqlPool>) -> HttpResponse {
-	let user = get_user_by_username(username.to_string(), pool).await;
-
-	HttpResponse::Ok().json(user)
-}
-
-#[actix_web::main]
-async fn main() -> Result<(), io::Error> {
-	dotenv().ok();
-	let port: u16 = match std::env::var("PORT") {
-		Ok(port) => port.parse::<u16>().unwrap(),
-		Err(_error) => 8080,
-	};
-	let database_url: String = std::env::var("DATABASE_URL").unwrap();
-	let pool: MySqlPool = connect(database_url)
-		.await
-		.expect("Could not connect to database");
-	HttpServer::new(move || {
-		App::new()
-			.app_data(Data::new(pool.clone()))
-			.service(add_user)
-			.service(get_user)
-			.service(delete_user)
-	})
-	.bind(("127.0.0.1", port))?
-	.run()
-	.await
+    Ok(())
 }
