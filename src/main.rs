1ac7cbda8e3590cfaa3b73b9486f237a07e3fc61
José Berríos <surphury@gmail.com>
2022-07-24 13:34:48 -0400

    feat: add cors config

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,10 @@ use dotenv::dotenv;
 
 use database::connect;
 
+use actix_cors::Cors;
+
 use actix_web::web::Data;
-use actix_web::{App, HttpServer};
+use actix_web::{http, App, HttpServer};
 
 use sqlx::mysql::MySqlPool;
 
@@ -40,8 +42,16 @@ async fn main() -> Result<(), io::Error> {
         .expect("Could not connect to database");
 
     HttpServer::new(move || {
+        let cors = Cors::default()
+            .allowed_origin("http://localhost:5173/")
+            .allowed_methods(vec!["GET", "POST", "DELETE", "PUT"])
+            .allowed_headers(vec![http::header::AUTHORIZATION, http::header::ACCEPT])
+            .allowed_header(http::header::CONTENT_TYPE)
+            .max_age(3600);
+
         App::new()
             .app_data(Data::new(Db { pool: pool.clone() }))
+            .wrap(cors)
             .service(login)
             .service(get_tasks)
             .service(post_task)
