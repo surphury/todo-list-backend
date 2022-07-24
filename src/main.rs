e86a870bfc16be1f883a66d91ffe85aa97cdf433
José Berríos <surphury@gmail.com>
2022-07-24 15:13:10 -0400

    fix: doesn't allow to connect from local

--- src/main.rs
+++ src/main.rs
@@ -43,7 +43,7 @@ async fn main() -> Result<(), io::Error> {
 
     HttpServer::new(move || {
         let cors = Cors::default()
-            .allowed_origin("http://localhost:5173/")
+            .allowed_origin("http://localhost:5173")
             .allowed_methods(vec!["GET", "POST", "DELETE", "PUT"])
             .allowed_headers(vec![http::header::AUTHORIZATION, http::header::ACCEPT])
             .allowed_header(http::header::CONTENT_TYPE)
